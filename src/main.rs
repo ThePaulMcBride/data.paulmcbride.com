a45f64142abb6880595902538b154e1e0659faad
Paul McBride <hello@paulmcbride.com>
2024-01-11 20:29:45 +0000

    remove unused route

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,8 @@ use axum::{routing::get, Router};
 mod health_check;
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
-        .nest("/health-check", health_check::health_check_router());
+    let app = Router::new().nest("/health-check", health_check::health_check_router());
 
-    // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
